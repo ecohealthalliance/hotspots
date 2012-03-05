7444262499402a4b4a8f3da7b8a3eadd8896e77e
Nico Preston <nicopresto@gmail.com>
2012-03-05 17:06:54 -0500

    commented code and removed hsd

--- regression.R
+++ regression.R
@@ -13,24 +13,36 @@ library(foreign) # exporting DBF files
 
 dr<-read.table(file="data/eid08_drivers_19OCT11.csv",sep=",",header=TRUE)
 
-#repeat regression analysis and scaling according to Jones et al 2008 to verify methods
-#gets about 100% correspondance to the original values and coef are roughly in the right range
+# repeat regression analysis and scaling according to Jones et al 2008 to verify methods
+# gets about 100% correspondance to the original values and coef are roughly in the right range
 
-#this was the weight used in the original analysis - a better one would be %land area in each grid cell
+# this was the weight used in the original analysis - a better one would be %land area in each grid cell
 weight=dr$landarea/mean(dr$landarea)
 abslat=abs(dr$lat)
-#Total
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Total GLM model (all drivers and all EIDs)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Total model
 mt<-glm(anytotr1~lndensity+lnpubs+high_pop_g+mamdiv+precip+abslat, family=binomial,weights=weight, data=dr)
 
+# Remove the publication parameter for the predictive model & calculate predictions
+# Note: cannot use the predict function because of the missing parameter
 pred_totr<-vector("numeric",nrow(dr))
 pred_totr<-1/(1+exp(-(coef(mt)[[1]]+coef(mt)[[2]]*dr$lndensity+coef(mt)[[4]]*dr$high_pop_g+coef(mt)[[5]]*dr$mamdiv+coef(mt)[[6]]*dr$precip+coef(mt)[[7]]*abslat))) 
 
+# Scale predictions between 1 and 0
 P_totr1<-1-1/(1+exp(pred_totr))
 P_totr1_sc<-(P_totr1-min(P_totr1))/(max(P_totr1)-min(P_totr1))
 
+# Check correlation between predicted output and original SPSS predictions
 cor.test(P_totr1_sc,dr$P_totr1_sc)
 
-#zoo
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Zoonotic non-wildlife disease (reponse variable)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 mz<-glm(anyzoor1~lndensity+lnpubs+high_pop_g+mamdiv+precip+abslat, family=binomial,weights=weight, data=dr)
 
 pred_totr<-vector("numeric",nrow(dr))
@@ -40,8 +52,10 @@ P_totr1<-1-1/(1+exp(pred_totr))
 P_totr1_sc<-(P_totr1-min(P_totr1))/(max(P_totr1)-min(P_totr1))
 
 cor.test(P_totr1_sc,dr$P_zoor1_sc)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Wildlife zoonotic disease (response variable)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#wild
 mw<-glm(anywildr1~lndensity+lnpubs+high_pop_g+mamdiv+precip+abslat, family=binomial,weights=weight, data=dr)
 
 pred_totr<-vector("numeric",nrow(dr))
@@ -52,7 +66,10 @@ P_totr1_sc<-(P_totr1-min(P_totr1))/(max(P_totr1)-min(P_totr1))
 
 cor.test(P_totr1_sc,dr$P_wildr1_s)
 
-#foodborne
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Foodborne disease (response variable)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 mf<-glm(anyfoodr1~lndensity+lnpubs+high_pop_g+mamdiv+precip+abslat, family=binomial,weights=weight, data=dr)
 
 pred_totr<-vector("numeric",nrow(dr))
@@ -63,7 +80,10 @@ P_totr1_sc<-(P_totr1-min(P_totr1))/(max(P_totr1)-min(P_totr1))
 
 cor.test(P_totr1_sc,dr$P_foodr1_s)
 
-#vector
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Vector-borne diseases (response variable)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 mv<-glm(anyvectr1~lndensity+lnpubs+high_pop_g+mamdiv+precip+abslat, family=binomial,weights=weight, data=dr)
 
 pred_totr<-vector("numeric",nrow(dr))
@@ -74,7 +94,10 @@ P_totr1_sc<-(P_totr1-min(P_totr1))/(max(P_totr1)-min(P_totr1))
 
 cor.test(P_totr1_sc,dr$P_vectr1_s)
 
-#drug res
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Drug resistant diseases (response variable)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 md<-glm(anydrmr1~lndensity+lnpubs+high_pop_g+mamdiv+precip+abslat, family=binomial,weights=weight, data=dr)
 
 pred_totr<-vector("numeric",nrow(dr))
@@ -85,25 +108,32 @@ P_totr1_sc<-(P_totr1-min(P_totr1))/(max(P_totr1)-min(P_totr1))
 
 cor.test(P_totr1_sc,dr$P_drmr1_sc)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# data mining with new drivers...
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# land use, pasture, livestock
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# calc new drivers
+luc <- dr$crop2000-dr$crop1900  # land-use change crop
+pc <- dr$pastur2000-dr$pastur1900  # pasture change
 
-#data mining with new drivers...
-luc<-dr$crop2000-dr$crop1900
-pc<-dr$pastur2000-dr$pastur1900
-
-lst<-(dr$goatct2000/max(dr$goatct2000)+dr$bfloct2000/max(dr$bfloct2000)+dr$ctlect2000/max(dr$ctlect2000)+dr$shpct2000/max(dr$shpct2000)+dr$pltyct2000/max(dr$pltyct2000)+dr$pigct2000/max(dr$pigct2000))/max(dr$goatct2000/max(dr$goatct2000)+dr$bfloct2000/max(dr$bfloct2000)+dr$ctlect2000/max(dr$ctlect2000)+dr$shpct2000/max(dr$shpct2000)+dr$pltyct2000/max(dr$pltyct2000)+dr$pigct2000/max(dr$pigct2000))
-
-#tot
+# Total model + landuse + pasture + various livestock
 m3<-glm(anytotr1~lndensity+high_pop_g+mamdiv+precip+luc+pc+dr$ctlect2000+dr$pigct2000+dr$shpct2000+dr$goatct2000+dr$bfloct2000 +dr$pltyct2000+GDPcap, family=binomial,weights=weight, data=dr)
 
+# Predictions
 pred_totr3<-vector("numeric",nrow(dr))
 pred_totr3<-1/(1+exp(-(coef(m3)[[1]]+coef(m3)[[2]]*dr$lndensity+coef(m3)[[3]]*dr$high_pop_g+coef(m3)[[4]]*dr$mamdiv+coef(m3)[[5]]*dr$precip+coef(m3)[[6]]*luc+coef(m3)[[7]]*dr$ctlect2000+coef(m3)[[8]]*dr$pigct2000+coef(m3)[[9]]*dr$shpct2000)))
 
+# Scaling
 P_totr3<-(1-1/(1+exp(pred_totr3)))
 P_totr3_sc<-(P_totr3-min(P_totr3))/(max(P_totr3)-min(P_totr3))
 dr$P_totr3_sc<-P_totr3_sc
 
-#zoo
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Response variable (non-wildlife zoonotic disease)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 m2z<-glm(anyzoor1~lndensity+high_pop_g+mamdiv+precip+luc+pc+dr$ctlect2000+dr$pigct2000+dr$shpct2000+dr$goatct2000+ dr$bfloct2000 +dr$pltyct2000+GDPcap, family=binomial,weights=weight, data=dr)
 
 pred_zoor3<-vector("numeric",nrow(dr))
@@ -113,7 +143,10 @@ P_zoor3<-(1-1/(1+exp(pred_zoor3)))
 P_zoor3_sc<-(P_zoor3-min(P_zoor3))/(max(P_zoor3)-min(P_zoor3))
 dr$P_zoor3_sc<-P_zoor3_sc
 
-#wild
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Response variable (wildlife zoonotic disease)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 m2w<-glm(anywildr1~lndensity+high_pop_g+mamdiv+precip+luc+dr$ctlect2000+dr$pigct2000+dr$shpct2000+GDPcap, family=binomial,weights=weight, data=dr)
 
 pred_wildr3<-vector("numeric",nrow(dr))
@@ -123,7 +156,10 @@ P_wildr3<-(1-1/(1+exp(pred_wildr3)))
 P_wildr3_sc<-(P_wildr3-min(P_wildr3))/(max(P_wildr3)-min(P_wildr3))
 dr$P_wildr3_sc<-P_wildr3_sc
 
-#vector
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Response variable (vector disease)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 m2v<-glm(anyvectr1~lndensity+high_pop_g+mamdiv+precip+luc+dr$ctlect2000+dr$pigct2000+dr$shpct2000+GDPcap, family=binomial,weights=weight, data=dr)
 
 pred_vectr3<-vector("numeric",nrow(dr))
@@ -133,7 +169,10 @@ P_vectr3<-(1-1/(1+exp(pred_vectr3)))
 P_vectr3_sc<-(P_vectr3-min(P_vectr3))/(max(P_vectr3)-min(P_vectr3))
 dr$P_vectr3_sc<-P_vectr3_sc
 
-#food
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Response variable (foodborne disease)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 m2f<-glm(anyfoodr1~lndensity+high_pop_g+mamdiv+precip+luc+dr$ctlect2000+dr$pigct2000+dr$shpct2000+GDPcap, family=binomial,weights=weight, data=dr)
 
 pred_foodr3<-foodor("numeric",nrow(dr))
@@ -143,7 +182,10 @@ P_foodr3<-(1-1/(1+exp(pred_foodr3)))
 P_foodr3_sc<-(P_foodr3-min(P_foodr3))/(max(P_foodr3)-min(P_foodr3))
 dr$P_foodr3_sc<-P_foodr3_sc
 
-#drug res
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Response variable (drug-resistant disease)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 m2d<-glm(anydrmr1~lndensity+high_pop_g+mamdiv+precip+luc+dr$ctlect2000+dr$pigct2000+dr$shpct2000+GDPcap, family=binomial,weights=weight, data=dr)
 
 pred_drmr3<-drmor("numeric",nrow(dr))
@@ -154,10 +196,15 @@ P_drmr3_sc<-(P_drmr3-min(P_drmr3))/(max(P_drmr3)-min(P_drmr3))
 dr$P_drmr3_sc<-P_drmr3_sc
 
 #write table with new models
+write.table(dr,"data/dr_new2.txt", sep=",")
 
-write.table(dr,"/Users/tiffzoo/Dropbox/Tiff Laptop Files/EcoHealth Alliance/Hotspots Analysis/Database files Feb 2011/New analysis/dr_new2.txt", sep=",")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Response variable (non-wildlife disease)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#zoor
+# combined index for livestock
+lst<-(dr$goatct2000/max(dr$goatct2000)+dr$bfloct2000/max(dr$bfloct2000)+dr$ctlect2000/max(dr$ctlect2000)+dr$shpct2000/max(dr$shpct2000)+dr$pltyct2000/max(dr$pltyct2000)+dr$pigct2000/max(dr$pigct2000))/max(dr$goatct2000/max(dr$goatct2000)+dr$bfloct2000/max(dr$bfloct2000)+dr$ctlect2000/max(dr$ctlect2000)+dr$shpct2000/max(dr$shpct2000)+dr$pltyct2000/max(dr$pltyct2000)+dr$pigct2000/max(dr$pigct2000))
+
 m3<-glm(anyzoor1~lndensity+high_pop_g+mamdiv+precip+lst+luc+pc+lnpubs+GDPcap, family=binomial,weights=weight, data=dr)
 
 pred_zoor3<-vector("numeric",nrow(dr))
@@ -166,21 +213,21 @@ pred_zoor3<-1/(1+exp(-(coef(m3)[[1]]+coef(m3)[[2]]*dr$lndensity+coef(m3)[[3]]*dr
 P_zoor3<-(1-1/(1+exp(pred_totr3)))
 P_zoor3_sc<-(P_totr3-min(P_totr3))/(max(P_totr3)-min(P_totr3))
 dr$P_zoor3_sc<-P_totr3_sc
-write.table(drzoo,"/Users/tiffzoo/Dropbox/Tiff Laptop Files/EcoHealth Alliance/Hotspots Analysis/Database files Feb 2011/New analysis/dr_zoo.txt", sep=",")
+write.table(drzoo,"data/dr_zoo.txt", sep=",")
 
-#wildr
+# todo(EHA) wildr
 
-#foodr
+# todo(EHA) foodr
 
-#drmr
+# todo(EHA) drmr
 
-#vectr
+# todo(EHA) vectr
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# new grid selection
 
-#new grid selection
-
-#read species information
-#only those in the nature paper
+# read species information
+# only those in the nature paper
 sp08<-as.data.frame(read.table("/Users/tiffzoo/Dropbox/Tiff Laptop Files/Manuscripts/Drivers of EIDs/Driver_analysis_March2011/sppnat08.TXT",header=TRUE,sep="\t"))
 sp08$nat08=1
 
